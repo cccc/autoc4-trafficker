e45c994a1b1e0fb591c5e482b9f750a3f3791862
fwam <contact@fwam.fyi>
2025-01-19 00:13:18 +0100

    Renamed main.py to trafficker-daemon.py

--- trafficker-daemon.py
+++ trafficker-daemon.py
@@ -8,9 +8,14 @@ from pyhafas import HafasClient
 from pyhafas.client import StationBoardLeg
 from pyhafas.profile import VSNProfile, KVBProfile
 import datetime
+import time
 import json
+import sys
 
 from typing import List, TypedDict, Dict
+
+import paho.mqtt.publish as publish 
+import paho.mqtt.client as mqtt
 
 # For typing purposes
 class JsonLayout(TypedDict):
@@ -32,14 +37,14 @@ class Trafficker:
     # TODO: Make the API requests asynchronously so that the required time gets down to a third.
     def _get_departures(self) -> List[StationBoardLeg]:
         departures_ehrenfeld: List[StationBoardLeg] = self.client_vsn.arrivals(
-            station="9406535", date=datetime.datetime.now() - datetime.timedelta(minutes=15), max_trips=10
+            station="9406535", date=datetime.datetime.now() - datetime.timedelta(minutes=15), max_trips=15
         )
 
         departures_venloerstr: List[StationBoardLeg] = self.client_vsn.arrivals(
             station=self.client_vsn.locations("Köln Venloer Str")[0],
             products={"bus": True},
             date=datetime.datetime.now(),
-            max_trips=10,
+            max_trips=15,
         )
 
         departures_venloerstr_bus: List[StationBoardLeg] = self.client_kvb.arrivals(
@@ -51,20 +56,30 @@ class Trafficker:
                 "regionalverkehr": False,
                 "fernverkehr": False,
             },
-            max_trips=10,
+            max_trips=15,
         )
 
         departures: List[StationBoardLeg] = departures_venloerstr + departures_ehrenfeld + departures_venloerstr_bus
         departures.sort(key=lambda dep: dep.dateTime)
         departures = self._clean_names_and_times(departures)
         departures = self._remove_invalid(departures)
-
-        distance_from_twenty: int = len(departures) - 20
-        if distance_from_twenty < 0:
-            distance_from_twenty = 0
-        for _ in range(distance_from_twenty):
+        departures = self._remove_past_connections(departures)
+        distance_from_sixteen: int = len(departures) - 16
+        if distance_from_sixteen < 0:
+            distance_from_sixteen = 0
+        for _ in range(distance_from_sixteen):
             departures.pop()
 
+        return departures
+    
+    def _remove_past_connections(self, departures: List[StationBoardLeg]) -> List[StationBoardLeg]:
+        past_connections = 0
+        for dpt in departures:
+            if (dpt.dateTime + (dpt.delay if dpt.delay is not None else datetime.timedelta(seconds=0))).astimezone(datetime.timezone.utc) < datetime.datetime.now(datetime.timezone.utc):
+                past_connections += 1
+        if past_connections != 0:
+            for _ in range(0,past_connections):
+                departures.pop(0)
         return departures
 
     def _clean_names_and_times(self, departures: List[StationBoardLeg]) -> List[StationBoardLeg]:
@@ -77,7 +92,7 @@ class Trafficker:
                     dpt.direction = dpt.direction.replace(phrase, "") # pyright: ignore
                     break
 
-            if dpt.platform == None:
+            if dpt.platform is None:
                 dpt.platform = "X"
         return departures
 
@@ -94,7 +109,7 @@ class Trafficker:
         json_output: JsonLayout = {"departures": [], "srvtime": "" }
         for dpt in self.departures:
             if dpt.delay is not None and dpt.delay.seconds != 0:
-                delay = f"+{str(dpt.delay.seconds // 60)}"
+                delay = f"+{str((dpt.delay.seconds // 60))}"
             else:
                 delay = ""
             departure = {
@@ -112,5 +127,21 @@ class Trafficker:
         return json.dumps(self.json_list, indent=2)
 
 if __name__ == "__main__":
-    json_output: str = Trafficker().output_json()
-    print(json_output)
+    mqttc = mqtt.Client(client_id="newtrafficker", callback_api_version=mqtt.CallbackAPIVersion.VERSION1)
+
+    def on_connect(a, b, flags, rc):
+        if rc != 0:
+            sys.exit(1)
+        else:
+            mqttc.publish('heartbeat/trafficker', bytearray(b'\x01'), retain=True)
+
+    mqttc.on_connect = on_connect
+    mqttc.will_set('heartbeat/trafficker', bytearray(b'\x00'), 2, True)
+    mqttc.connect("172.23.23.110",1883,60)
+    mqttc.loop_start()
+
+    while(True):
+        json_output: str = Trafficker().output_json()
+        publish.single('traffic/newdepartures', payload=json_output, hostname="autoc4", port=1883, keepalive=60)
+        time.sleep(5)
+
